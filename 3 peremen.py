2324c64dcff1b2c27a530d813e37cabb3b2e91cf
stanislavmeshkov <stasmtschkoff@gmail.com>
2024-08-23 14:17:03 +0300

    Add files via upload

---
+++ 3 peremen.py
@@ -0,0 +1,6 @@
+homework = 12
+hour = 1.5
+course = 'Python'
+time = (hour / homework)
+z = ','
+print('Курс:',course,z,'всего задач:',homework,z, 'затрачено часов:', hour,z, 'среднее время выполнения:', time, 'часа.')
